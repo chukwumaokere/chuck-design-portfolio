6f9a6bab21887d34228e7df4b663d6f8931acac4
chukwumaokere <chukwumaokere@yahoo.com>
2021-01-27 19:14:31 -0600

    update padding on about me section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center w-full mx-auto items-center sm:flex sm:flex-col sm:container sm:mx-auto sm:h-auto sm:items-center sm:justify-center sm:w-full">
-        <div class="pb-10 sm:flex">
+        <div class="sm:flex">
             <h1 class="circular-font text-4xl sm:text-4xl">About Me</h1>
         </div>
         <p class="py-10 text-xl sm:text-xl text-gray-400">Information to contact me is in my resume above!</p> 
