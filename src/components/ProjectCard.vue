a730b0a824bcccb1d5e28f8f4545fe469b1eacca
chukwumaokere <chukwumaokere@yahoo.com>
2021-01-27 19:16:36 -0600

    update functions

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ml-0 w-24 sm:w-52 bg-green-500 sm:bg-green-500 text-center rounded">
-        <img :src="project.placeholder_img && project.placeholder_img != '' ? project.placeholder_img : asset_placeholder_img " class="w-full cursor-pointer" @click="openDetailsModal(project.placeholder_img)" />
+        <img :src="project.placeholder_img && project.placeholder_img != '' ? project.placeholder_img : asset_placeholder_img " class="w-full cursor-pointer" @click="openImage(project.placeholder_img)" />
         <span class="text-white">{{project.name}}</span>
   </div>
 </template>
@@ -18,8 +18,10 @@ export default {
     },
     methods: {
         openDetailsModal(project){
-            //console.log('opening modal for', project)
-            window.open(project, "_blank")
+            console.log('opening modal for', project)
+        },
+        openImage(src){
+            window.open(src, "_blank")  
         }
     }
 }
