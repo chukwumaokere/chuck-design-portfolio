3c9f0c408dc798b7effd58594939e2e1ce72adf6
chukwumaokere <chukwumaokere@yahoo.com>
2023-04-11 01:35:26 -0500

    Update to use new resume

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,7 +14,7 @@
                     <li class="ml-10">Improving Developer Experience</li>
                 </ul>
                 <div class="flex justify-center pt-10 sm:pt-24 sm:block ">
-                    <a href="/documents/Stylized-Resume.pdf" @click="downloadCV" class="flex place-self-center sm:float-left px-4 py-3 sm:flex sm:px-5 sm:py-4 hover:shadow-lg shadow-md rounded transition ease-in duration-200 focus:outline-none hover:text-white text-white text-xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+                    <a href="/documents/Okere-Chuck-Resume.pdf" @click="downloadCV" class="flex place-self-center sm:float-left px-4 py-3 sm:flex sm:px-5 sm:py-4 hover:shadow-lg shadow-md rounded transition ease-in duration-200 focus:outline-none hover:text-white text-white text-xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
                         <svg class="w-6 h-6 sm:w-7 sm:h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
